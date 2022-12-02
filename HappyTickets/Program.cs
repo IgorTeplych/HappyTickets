6ceb25b6af7f401c3fd565376b4dd7d63cbeec79
Igor Teplych <igorvrt@mail.ru>
2022-12-02 20:37:47 +0600

    Реализовал алгоритм из журнала Квант 1976.

--- HappyTickets/Program.cs
+++ HappyTickets/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public static class Program
+{
+    static ulong[,] valueTables;
+    static ulong[] happyTickets;
+    static void Main()
+    {
+        var vr = Resolve(10);
+    }
+
+    public static ulong Resolve(int sizeTickets)
+    {
+        valueTables = new ulong[sizeTickets, 9 * sizeTickets + 1];
+        happyTickets = new ulong[sizeTickets];
+
+        //Заполненеие массива для n = 1
+        for (int k = 0; k < 10; k++)
+        {
+            valueTables[0, k] = 1;
+        }
+        //конец заполнения массива для n = 1
+
+        for (int n = 1; n < sizeTickets; n++)
+        {
+            for (int k = 9 * sizeTickets; k >= 0; k--)
+            {
+                happyTickets[n] += (ulong)Math.Pow(ShiftWindow(n, k), 2);
+            }
+        }
+        return happyTickets[sizeTickets - 1];
+    }
+
+
+
+    static ulong ShiftWindow(int n, int k)
+    {
+        for (int i = 0; (i <= k && i < 10); i++)
+        {
+            valueTables[n, k] += valueTables[n - 1, k - i];
+        }
+        return valueTables[n, k];
+    }
+
+}
